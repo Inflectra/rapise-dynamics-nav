a239406696471558b39ff2d467ed1a669b521511
dmarkovtsev <denis@inflectra.com>
2020-03-06 08:31:32 +0300

    Created during the webinar

---
+++ TC_CreateEmployees/Main.js
@@ -0,0 +1,11 @@
+
+
+
+function Test(params)
+{
+	RVL.DoPlayScript("%WORKDIR%\\TC_CreateEmployees\\Main.rvl.xlsx", "RVL");
+}
+
+g_load_libraries=["UIAutomation", "DynamicsNAV"];
+
+
---
+++ TC_CreateEmployees/Objects.js
@@ -0,0 +1,524 @@
+var saved_script_objects={
+	"New": {
+		"locations": [
+			{
+				"locator_name": "UIAutomation2",
+				"location": {
+					"location": [
+						{
+							"property": "ControlType",
+							"value": "Image"
+						},
+						{
+							"property": "Name",
+							"value": "New"
+						},
+						{
+							"property": "AutomationId",
+							"value": "PART_Image"
+						}
+					],
+					"window_name": "param:window_name",
+					"window_class": "regex:(WindowsForms10.)?Window.\\d+.app[a-z0-9_\\.]+"
+				}
+			},
+			{
+				"locator_name": "UIAutomation",
+				"location": {
+					"location": "param:location",
+					"window_name": "",
+					"window_class": "regex:HwndWrapper\\[Microsoft\\.Dynamics\\.Nav\\.Client\\.exe.+\\]"
+				}
+			}
+		],
+		"window_class": "WindowsForms10.Window.8.app.0.7b8f05_r6_ad1",
+		"object_text": "",
+		"object_role": "param:object_class",
+		"object_class": "Image",
+		"version": 0,
+		"object_type": "UIAObject",
+		"object_flavor": "UIAutomation",
+		"object_name": "New",
+		"ignore_object_name": true,
+		"object_library": "UIAutomation",
+		"window_name": "regex:.*Microsoft Dynamics NAV",
+		"location": "Pane/Custom/{FB8CCCF3-54AC-4d77-9AD8-136CEC25583A}/{17F878F7-1EB4-4B40-A6D5-55E87F26029B}/{27F878F7-1EB4-4B40-A6D5-55E87F26029B}/System.Windows.Controls.Ribbon.RibbonButton/{e00eecb1-ab9d-4d87-b763-6dd25509e067}/PART_Image"
+	},
+	"First_Name": {
+		"locations": [
+			{
+				"locator_name": "UIAutomation2",
+				"location": {
+					"location": [
+						{
+							"property": "ControlType",
+							"value": "Edit"
+						},
+						{
+							"property": "Name",
+							"value": "First Name"
+						},
+						{
+							"property": "AutomationId",
+							"value": "{00001450-0000-0004-0008-0000836BD2D2}"
+						}
+					],
+					"window_name": "param:window_name",
+					"window_class": "regex:(WindowsForms10.)?Window.\\d+.app[a-z0-9_\\.]+"
+				}
+			},
+			{
+				"locator_name": "UIAutomation",
+				"location": {
+					"location": "param:location",
+					"window_name": "param:window_name",
+					"window_class": "regex:(WindowsForms10.)?Window.\\d+.app[a-z0-9_\\.]+"
+				}
+			}
+		],
+		"window_class": "WindowsForms10.Window.8.app.0.7b8f05_r6_ad1",
+		"object_text": "",
+		"object_role": "param:object_class",
+		"object_class": "Edit",
+		"version": 0,
+		"object_type": "UIATextBox",
+		"object_flavor": "Text",
+		"object_name": "First Name",
+		"ignore_object_name": true,
+		"object_library": "UIAutomation",
+		"window_name": "regex:.* - Employee Card.*",
+		"location": "{00001450-0000-0000-0108-0000836BD2D2}/{00001450-0000-0000-0108-0000836BD2D2}/LayoutPlaceholder/content/contentAndMessagesPlaceholder/ContentPlaceholder/{00001450-0000-0001-0008-0000836BD2D2}/{00001450-0000-0004-0008-0000836BD2D2}[1]/{00001450-0000-0004-0008-0000836BD2D2}"
+	},
+	"Last_Name": {
+		"locations": [
+			{
+				"locator_name": "UIAutomation2",
+				"location": {
+					"location": [
+						{
+							"property": "ControlType",
+							"value": "Edit"
+						},
+						{
+							"property": "Name",
+							"value": "Last Name"
+						},
+						{
+							"property": "AutomationId",
+							"value": "{00001450-0000-0008-0008-0000836BD2D2}"
+						}
+					],
+					"window_name": "param:window_name",
+					"window_class": "regex:(WindowsForms10.)?Window.\\d+.app[a-z0-9_\\.]+"
+				}
+			},
+			{
+				"locator_name": "UIAutomation",
+				"location": {
+					"location": "param:location",
+					"window_name": "param:window_name",
+					"window_class": "regex:(WindowsForms10.)?Window.\\d+.app[a-z0-9_\\.]+"
+				}
+			}
+		],
+		"window_class": "WindowsForms10.Window.8.app.0.7b8f05_r6_ad1",
+		"object_text": "",
+		"object_role": "param:object_class",
+		"object_class": "Edit",
+		"version": 0,
+		"object_type": "UIATextBox",
+		"object_flavor": "Text",
+		"object_name": "Last Name",
+		"ignore_object_name": true,
+		"object_library": "UIAutomation",
+		"window_name": "regex:.* - Employee Card.*",
+		"location": "{00001450-0000-0000-0108-0000836BD2D2}/{00001450-0000-0000-0108-0000836BD2D2}/LayoutPlaceholder/content/contentAndMessagesPlaceholder/ContentPlaceholder/{00001450-0000-0001-0008-0000836BD2D2}/{00001450-0000-0008-0008-0000836BD2D2}[1]/{00001450-0000-0008-0008-0000836BD2D2}"
+	},
+	"Search_Name": {
+		"locations": [
+			{
+				"locator_name": "UIAutomation2",
+				"location": {
+					"location": [
+						{
+							"property": "ControlType",
+							"value": "Edit"
+						},
+						{
+							"property": "Name",
+							"value": "Search Name"
+						},
+						{
+							"property": "AutomationId",
+							"value": "{00001450-0000-001E-0008-0000836BD2D2}"
+						}
+					],
+					"window_name": "param:window_name",
+					"window_class": "regex:(WindowsForms10.)?Window.\\d+.app[a-z0-9_\\.]+"
+				}
+			},
+			{
+				"locator_name": "UIAutomation",
+				"location": {
+					"location": "param:location",
+					"window_name": "param:window_name",
+					"window_class": "regex:(WindowsForms10.)?Window.\\d+.app[a-z0-9_\\.]+"
+				}
+			}
+		],
+		"window_class": "WindowsForms10.Window.8.app.0.7b8f05_r6_ad1",
+		"object_text": "",
+		"object_role": "param:object_class",
+		"object_class": "Edit",
+		"version": 0,
+		"object_type": "UIATextBox",
+		"object_flavor": "Text",
+		"object_name": "Search Name",
+		"ignore_object_name": true,
+		"object_library": "UIAutomation",
+		"window_name": "regex:.* - Employee Card.*",
+		"location": "{00001450-0000-0000-0108-0000836BD2D2}/{00001450-0000-0000-0108-0000836BD2D2}/LayoutPlaceholder/content/contentAndMessagesPlaceholder/ContentPlaceholder/{00001450-0000-0001-0008-0000836BD2D2}/{00001450-0000-001E-0008-0000836BD2D2}[1]/{00001450-0000-001E-0008-0000836BD2D2}"
+	},
+	"Expand_or_collapse": {
+		"locations": [
+			{
+				"locator_name": "UIAutomation",
+				"location": {
+					"location": "param:location",
+					"window_name": "param:window_name",
+					"window_class": "regex:(WindowsForms10.)?Window.\\d+.app[a-z0-9_\\.]+"
+				}
+			}
+		],
+		"window_class": "WindowsForms10.Window.8.app.0.7b8f05_r6_ad1",
+		"object_text": "",
+		"object_role": "param:object_flavor",
+		"object_class": "param:object_flavor",
+		"version": 0,
+		"object_type": "UIAButton",
+		"object_flavor": "Button",
+		"object_name": "Expand or collapse",
+		"ignore_object_name": true,
+		"object_library": "UIAutomation",
+		"window_name": "regex:.* - Employee Card.*",
+		"location": "{00001450-0000-0000-0108-0000836BD2D2}/{00001450-0000-0000-0108-0000836BD2D2}/LayoutPlaceholder/content/contentAndMessagesPlaceholder/ContentPlaceholder/{00001450-716A-F1D9-0008-0000836BD2D2}/header/Expand or collapse"
+	},
+	"Extension": {
+		"locations": [
+			{
+				"locator_name": "UIAutomation2",
+				"location": {
+					"location": [
+						{
+							"property": "ControlType",
+							"value": "Edit"
+						},
+						{
+							"property": "Name",
+							"value": "Extension"
+						},
+						{
+							"property": "AutomationId",
+							"value": "{00001450-0000-0048-0008-0000836BD2D2}"
+						}
+					],
+					"window_name": "param:window_name",
+					"window_class": "regex:(WindowsForms10.)?Window.\\d+.app[a-z0-9_\\.]+"
+				}
+			},
+			{
+				"locator_name": "UIAutomation",
+				"location": {
+					"location": "param:location",
+					"window_name": "param:window_name",
+					"window_class": "regex:(WindowsForms10.)?Window.\\d+.app[a-z0-9_\\.]+"
+				}
+			}
+		],
+		"window_class": "WindowsForms10.Window.8.app.0.7b8f05_r6_ad1",
+		"object_text": "",
+		"object_role": "param:object_class",
+		"object_class": "Edit",
+		"version": 0,
+		"object_type": "UIATextBox",
+		"object_flavor": "Text",
+		"object_name": "Extension",
+		"ignore_object_name": true,
+		"object_library": "UIAutomation",
+		"window_name": "regex:.* - Employee Card.*",
+		"location": "{00001450-0000-0000-0108-0000836BD2D2}/{00001450-0000-0000-0108-0000836BD2D2}/LayoutPlaceholder/content/contentAndMessagesPlaceholder/ContentPlaceholder/{00001450-716A-F1D9-0008-0000836BD2D2}/{00001450-0000-0048-0008-0000836BD2D2}[1]/{00001450-0000-0048-0008-0000836BD2D2}"
+	},
+	"Expand_or_collapse1": {
+		"locations": [
+			{
+				"locator_name": "UIAutomation",
+				"location": {
+					"location": "param:location",
+					"window_name": "param:window_name",
+					"window_class": "regex:(WindowsForms10.)?Window.\\d+.app[a-z0-9_\\.]+"
+				}
+			}
+		],
+		"window_class": "WindowsForms10.Window.8.app.0.7b8f05_r6_ad1",
+		"object_text": "",
+		"object_role": "param:object_flavor",
+		"object_class": "param:object_flavor",
+		"version": 0,
+		"object_type": "UIAButton",
+		"object_flavor": "Button",
+		"object_name": "Expand or collapse",
+		"ignore_object_name": true,
+		"object_library": "UIAutomation",
+		"window_name": "regex:.* - Employee Card.*",
+		"location": "{00001450-0000-0000-0108-0000836BD2D2}/{00001450-0000-0000-0108-0000836BD2D2}/LayoutPlaceholder/content/contentAndMessagesPlaceholder/ContentPlaceholder/{00001450-7151-67D1-0008-0000836BD2D2}/header/Expand or collapse"
+	},
+	"Birth_Date": {
+		"locations": [
+			{
+				"locator_name": "UIAutomation2",
+				"location": {
+					"location": [
+						{
+							"property": "ControlType",
+							"value": "Edit"
+						},
+						{
+							"property": "Name",
+							"value": "Birth Date"
+						},
+						{
+							"property": "AutomationId",
+							"value": "{00001450-0000-0042-0008-0000836BD2D2}"
+						}
+					],
+					"window_name": "param:window_name",
+					"window_class": "regex:(WindowsForms10.)?Window.\\d+.app[a-z0-9_\\.]+"
+				}
+			},
+			{
+				"locator_name": "UIAutomation",
+				"location": {
+					"location": "param:location",
+					"window_name": "param:window_name",
+					"window_class": "regex:(WindowsForms10.)?Window.\\d+.app[a-z0-9_\\.]+"
+				}
+			}
+		],
+		"window_class": "WindowsForms10.Window.8.app.0.7b8f05_r6_ad1",
+		"object_text": "",
+		"object_role": "param:object_class",
+		"object_class": "Edit",
+		"version": 0,
+		"object_type": "UIATextBox",
+		"object_flavor": "Text",
+		"object_name": "Birth Date",
+		"ignore_object_name": true,
+		"object_library": "UIAutomation",
+		"window_name": "regex:.* - Employee Card.*",
+		"location": "{00001450-0000-0000-0108-0000836BD2D2}/{00001450-0000-0000-0108-0000836BD2D2}/LayoutPlaceholder/content/contentAndMessagesPlaceholder/ContentPlaceholder/{00001450-7151-67D1-0008-0000836BD2D2}/{00001450-0000-0042-0008-0000836BD2D2}[1]/{00001450-0000-0042-0008-0000836BD2D2}"
+	},
+	"Button": {
+		"locations": [
+			{
+				"locator_name": "UIAutomation",
+				"location": {
+					"location": "param:location",
+					"window_name": "param:window_name",
+					"window_class": "regex:(WindowsForms10.)?Window.\\d+.app[a-z0-9_\\.]+"
+				}
+			}
+		],
+		"window_class": "WindowsForms10.Window.8.app.0.7b8f05_r6_ad1",
+		"object_text": "",
+		"object_role": "param:object_flavor",
+		"object_class": "param:object_flavor",
+		"version": 0,
+		"object_type": "UIAButton",
+		"object_flavor": "Button",
+		"object_name": "Button",
+		"ignore_object_name": true,
+		"object_library": "UIAutomation",
+		"window_name": "regex:.* - Employee Card.*",
+		"location": "{00001450-0000-0000-0108-0000836BD2D2}/{00001450-0000-0000-0108-0000836BD2D2}/LayoutPlaceholder/ActionBar/{B594146A-9CEF-4767-AE76-E9721CD6FB50}/Button"
+	},
+	"DataGridView": {
+		"locations": [
+			{
+				"locator_name": "UIAutomation2",
+				"location": {
+					"location": [
+						{
+							"property": "ControlType",
+							"value": "Table"
+						},
+						{
+							"property": "Name",
+							"value": "DataGridView"
+						},
+						{
+							"property": "AutomationId",
+							"value": "_DataGrid"
+						}
+					],
+					"window_name": "param:window_name",
+					"window_class": "regex:(WindowsForms10.)?Window.\\d+.app[a-z0-9_\\.]+"
+				}
+			},
+			{
+				"locator_name": "UIAutomation",
+				"location": {
+					"location": "param:location",
+					"window_name": "param:window_name",
+					"window_class": "regex:(WindowsForms10.)?Window.\\d+.app[a-z0-9_\\.]+"
+				}
+			}
+		],
+		"window_class": "WindowsForms10.Window.8.app.0.7b8f05_r6_ad1",
+		"object_text": "",
+		"object_role": "param:object_flavor",
+		"object_class": "param:object_flavor",
+		"version": 0,
+		"object_type": "DynamicsNAVTable",
+		"object_flavor": "Table",
+		"object_name": "DataGridView",
+		"ignore_object_name": true,
+		"object_library": "DynamicsNAV",
+		"window_name": "regex:.*Microsoft Dynamics NAV",
+		"location": "{91115A6C-BE32-4f71-8AA5-5979F9364B54}/{91115A6C-BE32-4f71-8AA5-5979F9364B54}/LayoutPlaceholder/{00001451-0000-0000-0108-0000836BD2D2}/contentAndMessagesPlaceholder/contentAndFilterPlaceholder/contentPlaceholder/{00001451-0000-0001-0008-0000836BD2D2}/_DataGrid"
+	},
+	"Delete": {
+		"locations": [
+			{
+				"locator_name": "UIAutomation2",
+				"location": {
+					"location": [
+						{
+							"property": "ControlType",
+							"value": "Image"
+						},
+						{
+							"property": "Name",
+							"value": "Delete"
+						},
+						{
+							"property": "AutomationId",
+							"value": "PART_Image"
+						}
+					],
+					"window_name": "param:window_name",
+					"window_class": "regex:(WindowsForms10.)?Window.\\d+.app[a-z0-9_\\.]+"
+				}
+			},
+			{
+				"locator_name": "UIAutomation",
+				"location": {
+					"location": "param:location",
+					"window_name": "",
+					"window_class": "regex:HwndWrapper\\[Microsoft\\.Dynamics\\.Nav\\.Client\\.exe.+\\]"
+				}
+			}
+		],
+		"window_class": "WindowsForms10.Window.8.app.0.7b8f05_r6_ad1",
+		"object_text": "",
+		"object_role": "param:object_class",
+		"object_class": "Image",
+		"version": 0,
+		"object_type": "UIAObject",
+		"object_flavor": "UIAutomation",
+		"object_name": "Delete",
+		"ignore_object_name": true,
+		"object_library": "UIAutomation",
+		"window_name": "regex:.*Microsoft Dynamics NAV",
+		"location": "Pane/Custom/{FB8CCCF3-54AC-4d77-9AD8-136CEC25583A}/{17F878F7-1EB4-4B40-A6D5-55E87F26029B}/{37F878F7-1EB4-4B40-A6D5-55E87F26029B}/System.Windows.Controls.Ribbon.RibbonButton[2]/{da202066-7108-488d-b851-aee10468c5dc}/PART_Image"
+	},
+	"Yes": {
+		"locations": [
+			{
+				"locator_name": "UIAutomation2",
+				"location": {
+					"location": [
+						{
+							"property": "ControlType",
+							"value": "Button"
+						},
+						{
+							"property": "Name",
+							"value": "Yes"
+						},
+						{
+							"property": "AutomationId",
+							"value": "{ED4B78B8-F937-4e40-925E-639BAD0329C6}"
+						}
+					],
+					"window_name": "param:window_name",
+					"window_class": "regex:(WindowsForms10.)?Window.\\d+.app[a-z0-9_\\.]+"
+				}
+			},
+			{
+				"locator_name": "UIAutomation",
+				"location": {
+					"location": "param:location",
+					"window_name": "param:window_name",
+					"window_class": "regex:(WindowsForms10.)?Window.\\d+.app[a-z0-9_\\.]+"
+				}
+			}
+		],
+		"window_class": "WindowsForms10.Window.8.app.0.7b8f05_r6_ad1",
+		"object_text": "",
+		"object_role": "param:object_flavor",
+		"object_class": "param:object_flavor",
+		"version": 0,
+		"object_type": "UIAButton",
+		"object_flavor": "Button",
+		"object_name": "Yes",
+		"ignore_object_name": true,
+		"object_library": "UIAutomation",
+		"window_name": "regex:.*Microsoft Dynamics NAV",
+		"location": "00000000-0000-0000-0400-0000836bd2d2/00000000-0000-0000-0400-0000836bd2d2/LayoutPlaceholder/dialogActionBar/{ED4B78B8-F937-4e40-925E-639BAD0329C6}"
+	},
+	"Job_Title": {
+		"locations": [
+			{
+				"locator_name": "UIAutomation2",
+				"location": {
+					"location": [
+						{
+							"property": "ControlType",
+							"value": "Edit"
+						},
+						{
+							"property": "Name",
+							"value": "Job Title"
+						},
+						{
+							"property": "AutomationId",
+							"value": "{00001450-0000-000C-0008-0000836BD2D2}"
+						}
+					],
+					"window_name": "param:window_name",
+					"window_class": "regex:(WindowsForms10.)?Window.\\d+.app[a-z0-9_\\.]+"
+				}
+			},
+			{
+				"locator_name": "UIAutomation",
+				"location": {
+					"location": "param:location",
+					"window_name": "param:window_name",
+					"window_class": "regex:(WindowsForms10.)?Window.\\d+.app[a-z0-9_\\.]+"
+				}
+			}
+		],
+		"window_class": "WindowsForms10.Window.8.app.0.7b8f05_r6_ad1",
+		"object_text": "",
+		"object_role": "param:object_class",
+		"object_class": "Edit",
+		"version": 0,
+		"object_type": "UIATextBox",
+		"object_flavor": "Text",
+		"object_name": "Job Title",
+		"ignore_object_name": true,
+		"object_library": "UIAutomation",
+		"window_name": "regex:.* - Employee Card.*",
+		"location": "{00001450-0000-0000-0108-0000836BD2D2}/{00001450-0000-0000-0108-0000836BD2D2}/LayoutPlaceholder/content/contentAndMessagesPlaceholder/ContentPlaceholder/{00001450-0000-0001-0008-0000836BD2D2}/{00001450-0000-000C-0008-0000836BD2D2}[1]/{00001450-0000-000C-0008-0000836BD2D2}"
+	}
+};
